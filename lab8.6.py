5661838956cd7f166cf27d598c14e0016ae3ec8e
vaiahnavip <vaishnavisavlaj@gmail.com>
2026-07-30 14:00:59 +0530

    Add files via upload

---
+++ lab8.6.py
@@ -0,0 +1,10 @@
+
+for i in range(1,5):
+    for j in range (i):
+        print(j+1,end=" ")
+    print()
+
+for i in range(5,0,-1):
+    for j in range (i):
+        print(j+1,end=" ")
+    print()
