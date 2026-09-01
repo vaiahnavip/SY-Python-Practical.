c9d87da53d8a229e94f1f4c14ba9ec12d97c02d8
vaiahnavip <vaishnavisavlaj@gmail.com>
2026-09-01 13:29:47 +0530

    Add files via upload

---
+++ lab3.1.py
@@ -0,0 +1,9 @@
+age = int(input("Enter your age:"))
+income = float(input("Enter Your Annual Family Income:"))
+
+if age < 25 and income < 300000:
+    print("\n You are Eligible for the Scholarship.")
+
+else:
+    print("\n Sorry! You are not Eligible for the Scholarship.")
+
