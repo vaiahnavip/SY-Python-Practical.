df7d163504fc066a5d77fc1b9d7f0ec1b0353fea
vaiahnavip <vaishnavisavlaj@gmail.com>
2026-09-08 12:34:49 +0530

    Delete lab3.2.py

--- lab3.2.py
+++
@@ -1,9 +0,0 @@
-score = float(input("Enter Graduation Score :"))
-backlogs = int(input("Enter number of active backlogs :"))
-
-if score >= 70 and backlogs == 0:
-    print("\n Candidate is Eligible for Placement .")
-else:
-    print("\n Candidate is not Eligible for Placement .")
-
-
